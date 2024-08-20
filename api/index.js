a9e4ab95f03445e99989b5afd25df20985043634
raymonsadhra <raymonsadhra@gmail.com>
2024-08-20 11:30:27 -0700

    mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import configDotenv from 'dotenv';
+
+configDotenv.config();
+
+mongoose.connect(process.env.MONGO).then( () => { console.log('MongoDb is connected') }, )
 
 const app = express();
 app.listen(3000, () =>{    
