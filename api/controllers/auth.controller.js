440ecdf15798790c018a1a19728c2d58f39ed2ee
raymonsadhra <raymonsadhra@gmail.com>
2024-08-26 14:01:11 -0700

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res) => {
@@ -27,4 +28,33 @@ export const signup = async (req, res) => {
 
     await newUser.save();
     res.json('signup succesful');
+};
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return res.status(404).json({ message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest} = validUser._doc
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
