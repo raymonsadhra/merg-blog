063d69c923d61e884e728359199b42b1ba3e8583
raymonsadhra <raymonsadhra@gmail.com>
2024-09-14 13:58:54 -0700

    sign out function

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,8 @@ import { Button, Dropdown, DropdownHeader, DropdownItem, Navbar, TextInput } fro
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
+
 
 
 export default function Header() {
@@ -12,6 +14,23 @@ export default function Header() {
     const dispatch = useDispatch();
     const {currentUser} = useSelector(state=>state.user);
     const { theme }  = useSelector((state) => state.theme); 
+    const handleSignout = async () =>{
+      try {
+        const res = await fetch('/api/user/signout',){
+          method:'POST',
+        });
+        const data = await res.json();
+        if(!res.ok){
+          console.log(data.message);
+        }
+        else{
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+        
+      }
+     }; 
   return (
     <Navbar className='border-b-2 flex justify-between items-center p-4'>
       {/* Logo and Blog Title */}
@@ -60,7 +79,7 @@ export default function Header() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            <Dropdown.Item>Sign Out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
 
           </Dropdown>
         ):
