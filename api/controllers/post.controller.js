e563a34d1acb7e1254cb546cfadbbf0e59fc2ce5
raymonsadhra <raymonsadhra@gmail.com>
2024-09-21 13:24:09 -0700

    get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,3 +28,41 @@ export const create = async (req,res,next)=>{
         
     }
 }
+export const getposts = async (req,res,next) => {
+    try {
+        const startIndex=parseInt(req.query.startIndex)||0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order==='asc' ? 1 : -1;
+        const posts = await Post.find();
+         ...(req.query.userId && {userId:req.query.user.Id}),
+         ...(req.query.slug && {category:req.query.slug}),
+         ...(req.query.postId && {_id: req.query.post.Id}),
+         ...(req.query.searchTerm && {
+            $or: ([
+                { title: {$regex: req.query.searchTerm, $options: 'i'}},
+                { content: {$regex: req.query.searchTerm, $options: 'i'}},
+            )]
+         },
+        }).sort({updatedAt: sortDirection }).skip(startIndex).limit(limit);
+
+        const totalPost=await Post.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullyear(),
+            now.getFullMonth()-1,
+            now.getDate()
+        )
+        const lastMOnthPosts = await Post.countDocuments({
+            createdAt: ($gte: oneMonthAgo),
+        });
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMOnthPosts,
+        });
+
+    } catch (error) {
+        next(error);
+        
+    }
+}
