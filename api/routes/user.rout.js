daa1616ccad862099da826bfd68daa9c2128a904
raymonsadhra <raymonsadhra@gmail.com>
2024-10-06 11:36:02 -0700

    get users api route

--- api/routes/user.rout.js
+++ api/routes/user.rout.js
@@ -7,6 +7,7 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('signout',signout);
+router.get('/getusers',verifyToken,getUsers);
 
 export default router;
 
